f7c759b60fda2ae5dbaaa97c5a0e990c23983851
Leon <leon.bruewer@web.de>
2018-09-13 12:33:36 +0200

    created address controller

--- CompanyApi/Controller/Address.cs
+++ CompanyApi/Controller/Address.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class Address
+    public class Address : Microsoft.AspNetCore.Mvc.Controller
     {
+        private Repository.Address repo = new Repository.Address();
+        [Route("api/[Controller]")]
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var retval = repo.GetModelList();
+
+            return Ok(retval);
+        }
     }
 }
