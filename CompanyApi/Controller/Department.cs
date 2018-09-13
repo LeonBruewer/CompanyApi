9efcbef2062ba382d0c4f223cfc9d139ab88400d
Leon <leon.bruewer@web.de>
2018-09-13 12:38:21 +0200

    created department controller

--- CompanyApi/Controller/Department.cs
+++ CompanyApi/Controller/Department.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class Department
+    public class Department : Microsoft.AspNetCore.Mvc.Controller
     {
+        private Repository.Department repo = new Repository.Department();
+
+        [Route("api/[Controller]")]
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var retval = repo.GetModelList();
+
+            return Ok(retval);
+        }
     }
 }
