7b008f82931eaec5ee0605a03259dfb3eae75cd3
Leon <leon.bruewer@web.de>
2018-09-13 12:27:13 +0200

    created controller classes

---
+++ CompanyApi/Controller/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyApi.Controller
+{
+    public class Address
+    {
+    }
+}
