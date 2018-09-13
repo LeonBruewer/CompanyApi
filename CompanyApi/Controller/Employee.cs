fdb80d20cf82ed1fa55c29ea3a7ba332c734bb05
Leon <leon.bruewer@web.de>
2018-09-13 09:33:41 +0200

    add mvc services to startup class

--- CompanyApi/Controller/Employee.cs
+++ CompanyApi/Controller/Employee.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class Employee
+    public class Employee : Microsoft.AspNetCore.Mvc.Controller
     {
+        [HttpGet("/Employee")]
+        public JsonResult GetEmployee()
+        {
+            return new JsonResult(new List<object>(){
+                new {id=1, name="name"}
+            });
+        }
     }
 }
--- CompanyApi/Startup.cs
+++ CompanyApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi
 {
@@ -15,6 +16,7 @@ namespace CompanyApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,8 @@ namespace CompanyApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
