a821d06078b248e34ffd2b4bfd90fe67f0df1649
Leon <leon.bruewer@web.de>
2018-09-13 17:44:12 +0200

    added method to handle post requests

--- CompanyApi/Controller/Address.cs
+++ CompanyApi/Controller/Address.cs
@@ -7,28 +7,45 @@ using Microsoft.AspNetCore.Http;
 
 namespace CompanyApi.Controller
 {
+    [Route("api/Address")]
     public class Address : Microsoft.AspNetCore.Mvc.Controller
     {
+        
         private Repository.Address repo = new Repository.Address();
-        [Route("api/[Controller]")]
+
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            List<Model.Address> retval;
+
+            retval = repo.GetModelList();
+            
+            if (retval.Count == 0)
+                return StatusCode(StatusCodes.Status204NoContent);
+
+            return Ok(retval);
+        }
+
         [HttpGet("{id}")]
         public IActionResult Get(int Id)
         {
             List<Model.Address> retval;
 
-            if (Id > 0)
-            {
-                retval = repo.GetById(Id);
-            }
-            else
-            {
-                retval = repo.GetModelList();
-            }
+            retval = repo.GetById(Id);
 
             if (retval.Count == 0)
                 return StatusCode(StatusCodes.Status204NoContent);
 
             return Ok(retval);
         }
+
+
+        [HttpPost()]
+        public IActionResult AddOrUpdate(Model.Address obj)
+        {
+            return Ok(obj);
+        }
+
+        
     }
 }
