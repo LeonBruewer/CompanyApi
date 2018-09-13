3f24c28cf5c6137e9c56e17a8f7c286ae0709178
Leon <leon.bruewer@web.de>
2018-09-13 12:39:12 +0200

    created employee controller

--- CompanyApi/Controller/Employee.cs
+++ CompanyApi/Controller/Employee.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class Employee
+    public class Employee : Microsoft.AspNetCore.Mvc.Controller
     {
+        private Repository.Employee repo = new Repository.Employee();
+
+        [Route("api/[Controller]")]
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var retval = repo.GetModelList();
+
+            return Ok(retval);
+        }
     }
 }
