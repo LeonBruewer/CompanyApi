88bba5c31500fc6ae0b016ea3929d137f4db257c
Leon <leon.bruewer@web.de>
2018-09-13 12:37:12 +0200

    created company controller

--- CompanyApi/Controller/Company.cs
+++ CompanyApi/Controller/Company.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class Company
+    public class Company : Microsoft.AspNetCore.Mvc.Controller
     {
+        private Repository.Company repo = new Repository.Company();
+
+        [Route("api/[Controller]")]
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var retval = repo.GetModelList();
+
+            return Ok(retval);
+        }
     }
 }
