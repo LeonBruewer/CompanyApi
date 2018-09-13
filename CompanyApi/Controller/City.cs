876e577b378470b0a13d879e39e626dd5d40bb82
Leon <leon.bruewer@web.de>
2018-09-13 12:35:51 +0200

    created city controller

--- CompanyApi/Controller/City.cs
+++ CompanyApi/Controller/City.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyApi.Controller
 {
-    public class City
+    public class City : Microsoft.AspNetCore.Mvc.Controller
     {
+        private Repository.City repo = new Repository.City();
+
+        [Route("api/[Controller]")]
+        [HttpGet()]
+        public IActionResult Get()
+        {
+            var retval = repo.GetModelList();
+
+            return Ok(retval);
+        }
     }
 }
