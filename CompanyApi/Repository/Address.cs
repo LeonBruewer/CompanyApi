00e314586f9e8730fe66e2104421e7efd203043e
Leon <leon.bruewer@web.de>
2018-09-13 17:43:15 +0200

    added addOrUpdate method to address repository class

--- CompanyApi/Repository/Address.cs
+++ CompanyApi/Repository/Address.cs
@@ -36,5 +36,28 @@ namespace CompanyApi.Repository
             param.Add("@Id", Id);
             return con.Query<Model.Address>(query, param).ToList();
         }
+
+        
+        public Model.Address AddOrUpdate(string[] Param)
+        {
+            string query = "dbo.spAddOrUpdateCity";
+            DynamicParameters param = new DynamicParameters();
+
+            for (int i = 0; i < Param.Length; i++)
+            {
+                if (Param[i] == "")
+                    Param[i] = null;
+            }
+
+            string PostalCode = Param[0];
+            string City = Param[1];
+            
+            param.Add("@PostalCode", PostalCode);
+            param.Add("@City", City);
+
+            var retvalue = con.QueryFirstOrDefault<Model.Address>(query, param, null, null, CommandType.StoredProcedure);
+
+            return retvalue;
+        }
     }
 }
