268a52291a6d0f619b3c567c85c021ec16254000
Leon <leon.bruewer@web.de>
2018-09-14 12:33:46 +0200

    added handling for post and put requests to other classes

--- CompanyApi/Controller/Department.cs
+++ CompanyApi/Controller/Department.cs
@@ -7,29 +7,49 @@ using Microsoft.AspNetCore.Http;
 
 namespace CompanyApi.Controller
 {
+    [Route("api/Department")]
     public class Department : Microsoft.AspNetCore.Mvc.Controller
     {
         private Repository.Department repo = new Repository.Department();
 
-        [Route("api/[Controller]")]
-        [HttpGet("{id}")]
-        public IActionResult Get(int Id)
+        [HttpGet()]
+        public IActionResult Get()
         {
             List<Model.Department> retval;
 
-            if (Id > 0)
-            {
-                retval = repo.GetById(Id);
-            }
-            else
-            {
-                retval = repo.GetModelList();
-            }
+            retval = repo.GetModelList();
 
             if (retval.Count == 0)
                 return StatusCode(StatusCodes.Status204NoContent);
 
             return Ok(retval);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int Id)
+        {
+            List<Model.Department> retval;
+
+            retval = repo.GetById(Id);
+
+            if (retval.Count == 0)
+                return StatusCode(StatusCodes.Status204NoContent);
+
+            return Ok(retval);
+        }
+
+        [HttpPost()]
+        public IActionResult Add([FromBody] Model.dto.DepartmentDto obj)
+        {
+            Model.Department newObj = repo.Add(obj);
+            return Ok(newObj);
+        }
+
+        [HttpPut()]
+        public IActionResult Update([FromBody] Model.dto.DepartmentDto obj)
+        {
+            Model.Department newObj = repo.Update(obj);
+            return Ok(newObj);
         }
     }
 }
