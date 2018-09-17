5d1bbc0a77640411c6b2cb92624db52c82f83c6b
Leon <leon.bruewer@web.de>
2018-09-17 17:40:03 +0200

    created dbcontext class and interface

---
+++ CompanyApi/Helper/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using System.Data;
+using System.Data.SqlClient;
+using CompanyApi.Interfaces;
+using CompanyApi.Model;
+
+namespace CompanyApi.Helper
+{
+    public class DbContext : IDbContext
+    {
+        private DbSettings _settings;
+        public DbContext(IOptions<DbSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public IDbConnection GetConnection()
+        {
+
+            var con = new SqlConnection(_settings.Connection);
+
+            return con;
+        }
+    }
+}
