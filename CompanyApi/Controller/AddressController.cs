3d4bb42e84ff303e138d0417918d8658b2ca720f
Leon <leon.bruewer@web.de>
2018-09-17 15:56:33 +0200

    injected address dependency

--- CompanyApi/Controller/AddressController.cs
+++ CompanyApi/Controller/AddressController.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using CompanyApi.Model;
+using CompanyApi.Model.dto;
+using CompanyApi.Interfaces;
 
 namespace CompanyApi.Controller
 {
     [Route("api/Address")]
     public class AddressController : Microsoft.AspNetCore.Mvc.Controller
     {
-        private Repository.AddressRepo _repo = Repository.AddressRepo.GetInstatnce();
+        IRepository<Address, AddressDto> _repo;
+
+        public AddressController(IRepository<Address, AddressDto> repo)
+        {
+            _repo = repo;
+        }
 
         [HttpGet()]
         public IActionResult Get()
         {
-            List<Model.Address> retval;
+            List<Address> retval;
             
             retval = _repo.GetModelList();
             
--- CompanyApi/Repository/AddressRepo.cs
+++ CompanyApi/Repository/AddressRepo.cs
@@ -5,29 +5,16 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
 using Dapper;
-
+using CompanyApi.Interfaces;
+using CompanyApi.Model;
+using CompanyApi.Model.dto;
 namespace CompanyApi.Repository
 {
-    public class AddressRepo
+    public class AddressRepo : IRepository<Address, AddressDto>
     {
         SqlConnection con = new SqlConnection(global::CompanyApi.Properties.Resources.tappqaConString);
 
-        private static AddressRepo _Instance;
-
-        public static AddressRepo GetInstatnce()
-        {
-            if (_Instance == null)
-                _Instance = new AddressRepo();
-
-            return _Instance;
-        }
-
-        private AddressRepo()
-        {
-
-        }
-
-        public List<Model.Address> GetModelList()
+        public List<Address> GetModelList()
         {
             string query = @"SELECT Id,
                                     PostalCode,
@@ -35,10 +22,10 @@ namespace CompanyApi.Repository
                                     Street
                             FROM viAddress";
 
-            return con.Query<Model.Address>(query).ToList();
+            return con.Query<Address>(query).ToList();
         }
 
-        public List<Model.Address> GetById(int Id)
+        public List<Address> GetById(int Id)
         {
             string query = @"SELECT Id,
                                     PostalCode,
@@ -50,20 +37,20 @@ namespace CompanyApi.Repository
 
             var param = new DynamicParameters();
             param.Add("@Id", Id);
-            return con.Query<Model.Address>(query, param).ToList();
+            return con.Query<Address>(query, param).ToList();
         }
 
-        public Model.dto.AddressDto Add(Model.dto.AddressDto model)
+        public AddressDto Add(AddressDto model)
         {
             return _AddOrUpdate(model);
         }
 
-        public Model.dto.AddressDto Update(Model.dto.AddressDto model)
+        public AddressDto Update(AddressDto model)
         {
             return _AddOrUpdate(model, model.Id);
         }
 
-        private Model.dto.AddressDto _AddOrUpdate(Model.dto.AddressDto model, object Id = null)
+        private AddressDto _AddOrUpdate(AddressDto model, object Id = null)
         {
             string query = "dbo.spAddOrUpdateAddress";
             DynamicParameters param = new DynamicParameters();
@@ -75,7 +62,7 @@ namespace CompanyApi.Repository
             param.Add("@PostalCode", PostalCode);
             param.Add("@Street", Street);
 
-            var retvalue = con.QueryFirstOrDefault<Model.dto.AddressDto>(query, param, null, null, CommandType.StoredProcedure);
+            var retvalue = con.QueryFirstOrDefault<AddressDto>(query, param, null, null, CommandType.StoredProcedure);
 
             return retvalue;
         }
